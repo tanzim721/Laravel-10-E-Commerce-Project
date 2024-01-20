a9594d8bc18111d180ad7121ec421ce893278bcc
Tanzimul Islam <53827851+tanzim721@users.noreply.github.com>
2024-01-20 12:07:21 +0600

    Frontend setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AppController::class, 'index'])->name('app.index');
 
 Auth::routes();
 
